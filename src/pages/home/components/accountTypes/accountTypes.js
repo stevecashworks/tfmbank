9a79f57ff67993879f603e4976b06fe23a70fecf
stevecashworks <chigbustephennamdi@gmail.com>
2023-09-17 12:23:50 +0100

    typescript removed

--- src/pages/home/components/accountTypes/accountTypes.js
+++ src/pages/home/components/accountTypes/accountTypes.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import Card from './Card'
-import responsive from '../../../../responsive.ts'
+import responsive from '../../../../responsive.js'
 const Con=styled.div`
  width:100%;
  height: 600px;
